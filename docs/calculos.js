efd456907c743a3e70c8e54ee402dc3a5c201b32
RhaynerRS <80178543+RhaynerRS@users.noreply.github.com>
2021-03-26 18:39:30 -0300

    Add files via upload

--- docs/calculos.js
+++ docs/calculos.js
@@ -1,58 +1,78 @@
-function IMC(){
-    var peso=document.getElementById("peso").value
-    var alt=document.getElementById("alt").value
-    var altura=alt/100
-  if (alt.indexOf('.' || ',')>-1){ imc=(peso/(alt*alt))}
-  else{imc=(peso/(altura*altura))}
-
- 
-  if (imc<17){
-    status="muito abaixo do peso."
-  }
-  else if (imc>=17 && imc<=18.49){
-    status="abaixo do peso."
-  }
-  else if (imc>=18.5 && imc<=24.99){
-    status="com o peso normal."
-  }
-  else if (imc>=25 && imc<=29.99){
-    status="Acima do peso."
-  }
-  else if (imc>=30 && imc<=34.99){
-    status="Obesidade I."
-  }
-  else if (imc>=35 && imc<=39.99){
-    status="Obesidade II."
-  }
-  else{
-    status="Obesidade III"
-  }
-       imco.innerHTML=imc.toFixed(2)
-       result.innerHTML=status}
+function IMC() {
+  var peso = document.getElementById("peso").value
+  var alt = document.getElementById("alt").value
+  var altura = alt / 100
+  if (alt.indexOf('.' || ',') > -1) { imc = (peso / (alt * alt)) }
+  else { imc = (peso / (altura * altura)) }
 
 
-function BF(){
-  var neck=document.getElementById("neck").value
-  var alt=document.getElementById("alt").value
-  var waist=document.getElementById("waist").value
-  var altura=alt/100
-  if (alt.indexOf('.' || ',')>-1){
-  bf=(495/(1.0324 - 0.19077*Math.log10(waist-neck)  + 0.15456*Math.log10(alt*100)))-450
-  result.innerHTML=bf.toFixed(2)}
-  else{bf=(495/(1.0324 - 0.19077*Math.log10(waist-neck)  + 0.15456*Math.log10(altura*100)))-450
-    result.innerHTML=bf.toFixed(2)}
+  if (imc < 17) {
+    status = "muito abaixo do peso."
+  }
+  else if (imc >= 17 && imc <= 18.49) {
+    status = "abaixo do peso."
+  }
+  else if (imc >= 18.5 && imc <= 24.99) {
+    status = "com o peso normal."
+  }
+  else if (imc >= 25 && imc <= 29.99) {
+    status = "Acima do peso."
+  }
+  else if (imc >= 30 && imc <= 34.99) {
+    status = "Obesidade I."
+  }
+  else if (imc >= 35 && imc <= 39.99) {
+    status = "Obesidade II."
+  }
+  else {
+    status = "Obesidade III"
+  }
+  imco.innerHTML = ("Seu imc é: " + imc.toFixed(2))
+  result.innerHTML = ("E voce esta: " + status)
+}
+
+
+function BF() {
+  var neck = document.getElementById("neck").value
+  var alt = document.getElementById("alt").value
+  var waist = document.getElementById("waist").value
+  var altura = alt / 100
+  if (alt.indexOf('.' || ',') > -1) {
+    bf = (495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(alt * 100))) - 450
+    result.innerHTML = bf.toFixed(2)
+  }
+  else {
+    bf = (495 / (1.0324 - 0.19077 * Math.log10(waist - neck) + 0.15456 * Math.log10(altura * 100))) - 450
+    result.innerHTML = ("Sua BF é de: " + bf.toFixed(2) + '%')
+  }
 
 }
 
-function TMB(){
-  var peso=document.getElementById("peso").value
-  var alt=document.getElementById("alt").value
-  var idade=document.getElementById("idade").value
-  var altura=alt/100
-  if (alt.indexOf('.' || ',')>-1){
-  tmb=(66+(13.8*peso)+(5*(alt*100)))-(6.8*idade)}
-  else{tmb=(66+(13.8*peso)+(5*(altura*100)))-(6.8*idade)
+function TMB() {
+  var peso = document.getElementById("peso").value
+  var alt = document.getElementById("alt").value
+  var idade = document.getElementById("idade").value
+  var sexo = document.getElementById("sexo").value
+  console.log(sexo)
+  var altura = alt / 100
+  console.log(alt)
+  if (sexo == "masculino") {
+    if (alt.indexOf('.' || ',') > -1) {
+      tmb = (66 + (13.8 * peso) + (5 * (alt * 100))) - (6.8 * idade)
+    }
+    else {
+      tmb = (66 + (13.8 * peso) + (5 * (altura * 100))) - (6.8 * idade)
+    }
   }
-  result.innerHTML=tmb.toFixed(2)
+  else {
+    if (alt.indexOf('.' || ',') > -1) {
+      tmb = (655 + (9.6 * peso) + (1.8 * (alt * 100))) - (4.7 * idade)
+    }
+    else {
+      tmb = (655 + (9.6 * peso) + (1.8 * (altura * 100))) - (4.7 * idade)
+    }
+  }
+
+  result.innerHTML = ("Sua TMB é de: " + tmb.toFixed(2))
 
 }
